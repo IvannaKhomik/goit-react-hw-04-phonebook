97d95fc8fb0411933dc320b347541ccaa7e7b6ca
IvannaKhomik <ivanna.khomik@gmail.com>
2022-10-31 13:43:12 +0200

    add data file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,10 @@ import { ContactForm } from './ContactForm';
 import { ContactList } from './ContactList';
 import { Container } from './Container';
 import { nanoid } from 'nanoid';
+import { defaultContacts } from '../utils/data';
 import { Filter } from './Filter/Filter';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import useLocalStorage from 'hooks/useLocalStorage';
-
-const defaultContacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
 
 export const App = () => {
   const [contacts, setContacts] = useLocalStorage('contacts', defaultContacts);
